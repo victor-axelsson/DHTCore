376cddef16335b86f0d143de9f97db279731a2c4
Victor Axelsson <victor_axelsson@hotmail.com>
2016-11-25 14:43:20 +0100

    Trying out finger keys

--- src/se/kth/networking/java/first/ring/FingerTable.java
+++ src/se/kth/networking/java/first/ring/FingerTable.java
@@ -1,38 +1,64 @@
 package se.kth.networking.java.first.ring;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import se.kth.networking.java.first.models.Node;
+import se.kth.networking.java.first.models.OnResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nick on 11/25/2016.
  */
 public class FingerTable {
-    public static long SUCCESSOR_OFFSET = 2L;
-    public static long EIGHTH_RING_OFFSET = 536870912L;
-    public static long QUARTER_RING_OFFSET = 1073741824L;
-    public static long HALF_RING_OFFSET = 2147483648L;
-    public static long RING_SIZE = 4294967296L;
 
     private Node self;
     private Node successor;
-    private Node eighth;
-    private Node quarter;
-    private Node half;
+    private List<Long> fingers;
+    private static final int RING_BIT_SIZE = 32;
 
     public FingerTable(Node self, Node successor) {
         this.self = self;
         this.successor = successor;
+        fingers = new ArrayList<>();
+        setupFingerKeys();
     }
 
-    public Node getNodeByKey(long key) {
-        long selfKey = self.getId();
-
-        if (selfKey == key) return self;
-        if ((selfKey + SUCCESSOR_OFFSET - 1) % RING_SIZE == key) return successor;
-        if ((selfKey + EIGHTH_RING_OFFSET - 1) % RING_SIZE == key) return eighth;
-        if ((selfKey + QUARTER_RING_OFFSET - 1) % RING_SIZE == key) return quarter;
-        if ((selfKey + HALF_RING_OFFSET - 1) % RING_SIZE == key) return half;
-
-        return null;
-        //todo check which node is closest
+    private void setupFingerKeys(){
+        for(int i = 0; i < RING_BIT_SIZE; i++){
+            fingers.add((self.getId() + 2^i - 1) % 2 ^ RING_BIT_SIZE);
+        }
     }
+
+    public String createFingerProbeMessage(){
+        JSONObject message = new JSONObject();
+        message.put("ip", self.getIp());
+        message.put("port", self.getPort());
+        message.put("type", "finger_probe");
+        message.put("keys", new JSONArray(fingers.toArray()));
+        message.put("fingers", new JSONArray());
+
+        return message.toString();
+    }
+
+    public String dealWithFingerProbe(String message, OnResponse<String> onDone){
+        JSONObject jsonMessage = new JSONObject(message);
+        List<Long> keys = (List<Long>)(List<?>)jsonMessage.getJSONArray("keys").toList();
+
+        if(keys.contains(self.getId())){
+            jsonMessage.getJSONArray("fingers").put(new JSONObject(self.toString()));
+            keys.remove(self.getId());
+            jsonMessage.remove("keys");
+            jsonMessage.put("keys", keys);
+        }
+
+        if(keys.isEmpty()){
+            Node sender = new Node(jsonMessage.toString());
+            onDone.onResponse(jsonMessage.toString(), sender);
+        }
+
+        return jsonMessage.toString();
+    }
+
 }
